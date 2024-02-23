2dd1fa18af44d72573576a7403851aacdddbdb7e
Indra d Gairhe <indgairhe@gmail.com>
2024-02-23 18:15:39 -0500

    test...

--- test.js
+++ test.js
@@ -1,34 +1,31 @@
-// is the number prime..? 
+// is the number prime..?
 
 function isPrime(n) {
+  // if any number above 1 and below n divides n without remainder, it returns false
 
-
-    // if any number above 1 and below n divides n without remainder, it returns false
-
-
-    let divisor = 2; 
-    while (n > divisor) {
-        if( n %divisor === 0) {
-            return false
-        }
-
-        divisor++ ;
+  let divisor = 2;
+  while (n > divisor) {
+    if (n % divisor === 0) {
+      return false;
     }
-    return true
+
+    divisor++;
+  }
+  return true;
 }
 
 function isPrimeTwo(n) {
-    let divisor = 2; 
-    while (divisor <= n/2) {
-        if( n % divisor === 0) {
-            return false
-        }
-
-        divisor++ ;
+  let divisor = 2;
+  while (divisor <= n / 2) {
+    if (n % divisor === 0) {
+      return false;
     }
-    return true
+
+    divisor++;
+  }
+  return true;
 }
+//// testing text ...
 
-
-let b = isPrimeTwo(237)
-console.log(b)
+let b = isPrimeTwo(237);
+console.log(b);
